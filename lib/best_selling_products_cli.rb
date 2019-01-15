29b1daf28e2bde0e710b626a006acfb9ecb4130c
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-01-15 16:24:58 +0000

    first commit

---
+++ lib/best_selling_products_cli.rb
@@ -0,0 +1,6 @@
+require "best_selling_products_cli/version"
+
+module BestSellingProductsCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/best_selling_products_cli/version.rb
@@ -0,0 +1,3 @@
+module BestSellingProductsCli
+  VERSION = "0.1.0"
+end
