9651b171130939f59f9666fbd1b4eab563804ed5
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-01-17 19:02:00 +0000

    added some functionality

--- lib/best_selling_products_cli/cli.rb
+++ lib/best_selling_products_cli/cli.rb
@@ -1,16 +1,53 @@
 class BestSellingProductsCLI::CLI
 
+
+
   def call
+    BestSellingProducts::Scraper.new.make_best_sellers
+    puts "Welcome to Best Selling Products on Amazon!"
+    start
   end
 
   def start
+    #show a list of the top 50 best selling proudcts
+    puts "Here is a list of the Top 50 best selling products:"
+    puts "What number best seller would you like to view? (enter 1 - 100)"
+    input = gets.strip.to_i
+
+    print_best_sellers(input)
+
+    puts "What best seller would you like to learn more about?"
+    input = gets.strip
+
+    best_seller = BestSellingProductsCLI::Best_Seller.find(input.to_i)
+
+    print_best_seller(best_seller)
+
+    puts "Would you like to view another best seller? Enter Y or N"
+
+    input = gets.strip.downcase
+    if input == "y"
+      start
+    elsif input == "n"
+      puts "Thank you for viewing the best selling products on Amazon! Have a great day!"
+      exit
+  end
+end
+    #scrape all of the products---call to the scraper class
+    #option to ask for input from user
+    #call another method based on user input  
   end
 
-  def print_best_seller
+  def list_products
+    #start our product list index at 1
+    BestSellingProductsCLI::Best_Seller.all.each.with_index(1) do | best_seller, index |
+      puts "#{index}. #{best_seller.name}"
   end
+end
 
-  def farewell
-  end
+
+
+
 
 
 end
--- lib/best_selling_products_cli/scraper.rb
+++ lib/best_selling_products_cli/scraper.rb
@@ -1,12 +1,15 @@
 class BestSellingProducts::Scraper
+#first level to scrape the top 50 best selling products
+
+
 
 def open_page
     Nokogiri::HTML(open("https://www.amazon.com/Best-Sellers/zgbs")) #open page
   end
-
+#first level to scrape the top 50 best selling products
   def scrape_products
   end
-
+#second level scrape
   def make_products
   end
 
