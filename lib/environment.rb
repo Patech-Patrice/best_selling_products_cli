a3b7671b722737d5cfcfa6da2cb8223076f7279f
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-01-17 17:58:35 +0000

    updated gem specs

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,4 +1,9 @@
 #This is our environment file where we will require all of the classes we build inside of our best_selling_products_cli directory.
+require "open-uri"
+require "nokogiri"
+require "pry"
 
 require_relative "./best_selling_products_cli/version"
 require_relative "./best_selling_products_cli/cli"
+require_relative "./best_selling_products_cli/scraper "
+require_relative "./best_selling_products_cli/best_seller
