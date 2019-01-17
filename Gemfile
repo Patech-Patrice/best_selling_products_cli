1285df25121098373407f448039a0b907672fd5e
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-01-17 22:31:31 +0000

    installed open-uri

--- Gemfile
+++ Gemfile
@@ -1,8 +1,5 @@
 source "https://rubygems.org"
 
+
 # Specify your gem's dependencies in best_selling_products_cli.gemspec
 gemspec
-
-gem 'nokogiri'
-gem 'open-uri'
-gem 'pry'
--- lib/environment.rb
+++ lib/environment.rb
@@ -1,6 +1,6 @@
 #This is our environment file where we will require all of the classes we build inside of our best_selling_products_cli directory.
-require 'nokogiri'
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 require_relative "../lib/best_selling_products_cli/version"
